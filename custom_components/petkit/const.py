ee63e4ff5d5894587c591af12960b1b69ff8d714
xiaohuo <et.tw@163.com>
2024-04-02 13:59:23 +0800

    支持YumShare版本喂食器

--- custom_components/petkit/const.py
+++ custom_components/petkit/const.py
@@ -346,7 +346,7 @@ FEEDERS = {
     'd3': 'Fresh Element Infinity',
     'd4': 'Fresh Element Solo',
     'd4s': 'Fresh Element Gemini',
-    'd4sh': 'Fresh Element Gemini',
+    'd4sh': 'Fresh Element YumShare',
     'feeder': 'Fresh Element',
     'feedermini': 'Fresh Element Mini Pro',
 }
